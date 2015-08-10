7e724894f1bf0b18e2c48990b55089579358eab9
ben-thul <ben.thul@gmail.com>
2015-08-09 17:05:43 -0700

    Added FormatCurrency

---
+++ SQLCLR/Scalar Functions/FormatCurrency.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using System.Globalization;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction]
+    public static SqlString FormatCurrency(SqlDouble Amount, SqlString culture)
+    {
+        string _culture;
+        if (culture.IsNull == true)
+        {
+            _culture = CultureInfo.CurrentCulture.Name;
+        }
+        else
+        {
+            _culture = culture.Value;
+        }
+        try {
+            CultureInfo ci = new CultureInfo(_culture);
+            return new SqlString(Amount.Value.ToString("C", ci));
+        }
+        catch (CultureNotFoundException e)
+        {
+            return new SqlString("Invalid culture");
+        }
+    }
+}
